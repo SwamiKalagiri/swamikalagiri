47cea373924e488aede8dcc3cfe7649e61704f14
Swami Kalagiri <KalagiriSwamiReddy@JohnDeere.com>
2018-12-20 20:38:08 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Swami Kalagiri
+Swami R Kalagiri's personal npm card: "npx swamikalagiri" 
+
+```bash
+npx swamikalagiri
+```
+
+![business card](/preview.png)
+
+---
---
+++ bin/cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+const swamikalagiri = require('../lib').default;
+
+console.log(swamikalagiri);
